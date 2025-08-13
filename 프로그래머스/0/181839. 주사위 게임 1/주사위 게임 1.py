599b4e1a4f06f899757da0767d95a71f8fcfe123
장여정 <yeojeong735@gmail.com>
2025-08-13 23:00:35 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.00 ms, Memory: 9.29 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.py
@@ -0,0 +1,11 @@
+def solution(a, b):
+    answer = 0
+    
+    if a % 2 == 1 and b % 2 == 1 :
+        answer = (a*a + b*b)
+    elif a % 2 == 1 or b % 2 == 1 :
+        answer = 2 * (a + b)
+    else :
+        answer = abs(a - b)
+        
+    return answer
